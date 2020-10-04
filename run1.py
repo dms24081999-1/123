7bbcd93736aa08008483cf6b76a22da3f86cfa81
Dominic Michael Silveira <dms24081999@gmail.com>
2020-10-04 15:54:22 +0530

    4

---
+++ run1.py
@@ -0,0 +1,4 @@
+print(input())
+print(input())
+print(int(input()))
+print(input())
--- test_generator.py
+++ test_generator.py
@@ -2,8 +2,32 @@ import unittest
 import subprocess
 
 def test_sample_single_word():
-    run=subprocess.Popen(["python3", "run.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    grep_stdout =run.communicate(input=b'one\ntwo\nthree\nfour\n')[0]
+    inf=open('in.txt','rb')
+    inputdata=inf.read()
+    inf.close()
+
+    otf=open('out.txt','rb')
+    outputdata=otf.read()
+    otf.close()
+    
+    run=subprocess.Popen(["python", "run.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    grep_stdout =run.communicate(input=inputdata)[0]
     print(grep_stdout.decode())
     print(run.returncode)
-    assert 0==1
+    assert bytes(grep_stdout.decode(),'utf-8')==outputdata
+    
+    
+def test_sample_single_word2():
+    inf=open('in.txt','rb')
+    inputdata=inf.read()
+    inf.close()
+
+    otf=open('out.txt','rb')
+    outputdata=otf.read()
+    otf.close()
+    
+    run=subprocess.Popen(["python", "run1.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    grep_stdout =run.communicate(input=inputdata)[0]
+    print(grep_stdout.decode())
+    print(run.returncode)
+    assert bytes(grep_stdout.decode(),'utf-8')==outputdata
