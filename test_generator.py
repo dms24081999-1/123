94d2b2fe9a5016abfaf8eb1101c2fdd3d36d55f8
Dominic Michael Silveira <dms24081999@gmail.com>
2020-10-04 15:26:59 +0530

    First

---
+++ test_generator.py
@@ -0,0 +1,9 @@
+import unittest
+import subprocess
+
+def test_sample_single_word():
+    run=subprocess.Popen(["python3", "run.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    grep_stdout =run.communicate(input=b'one\ntwo\nthree\nfour\n')[0]
+    print(grep_stdout.decode())
+    print(run.returncode)
+    assert 0==0
